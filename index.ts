8ec3d67e498cd9f4e1fb112e45c7bd55ebd13bfa
Len <len.joseph@permitdocs.com>
2020-06-22 00:09:01 -0700

    adjust generator params

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import writeDeviceData from './data/load/index';
 import { analyzeDeviceData } from './data/analyze/index';
 
 // generate data, perform write to disc with data separated by device source
-writeDeviceData(readDeviceData(5, 100), {
+writeDeviceData(readDeviceData(100, 5), {
 	thermoPath: './data/temp/thermoData.json',
 	accelPath: './data/temp/accelData.json',
 	photoPath: './data/temp/photoData.json',
