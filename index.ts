315354acab49e9b0b5bd14625ede496d3ea7e184
Len <len.joseph@permitdocs.com>
2020-06-23 10:51:09 -0700

    cover edge case of no device data for a device

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import writeDeviceData from './load/index';
 import analyzeDeviceData from './analyze/index';
 
 // generate data, perform write to disc with data separated by device source
-writeDeviceData(readDeviceData(2, 5), {
+writeDeviceData(readDeviceData(1000, 5), {
 	thermoPath: './temp/thermoData.json',
 	accelPath: './temp/accelData.json',
 	photoPath: './temp/photoData.json',
